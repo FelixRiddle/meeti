2e3c36162a97b16eb628269092f0960e0e179903
Felix <fedecraft3312@gmail.com>
2024-06-30 21:25:20 -0300

    Added some styles

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set(`views`, `${process.cwd()}/views`);
 
+app.use("/public", express.static("public"));
+
 app.use(mainRouter);
 
 app.listen(PORT, () => {

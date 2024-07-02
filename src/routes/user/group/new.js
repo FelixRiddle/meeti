09abbc07dd4d114f159b611967266b044e52b979
Felix <fedecraft3312@gmail.com>
2024-07-02 19:41:41 -0300

    Show categories

--- src/routes/user/group/new.js
+++ src/routes/user/group/new.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const expandData = require("../../../lib/misc/expandData");
+const { renderDataInternalErrorMessage } = require('../../../lib/status/messages');
 
 const newRouter = express.Router();
 
-newRouter.get("/", (req, res) => {
+newRouter.get("/", async (req, res) => {
+	const SocialCategory = req.models.SocialCategory;
+	
+	const categories = await SocialCategory.findAll({
+		raw: true,
+	});
+	console.log(`Categories: `, categories);
+	
 	return res.render("user/group/new", {
 		title: "Create new group",
 		...expandData(req),
+		categories,
 	});
+});
+
+newRouter.post("/", (req, res) => {
+	try {
+		
+	} catch(err) {
+		console.error(err);
+		return res
+			.status(500)
+			.render("status", {
+				...renderDataInternalErrorMessage
+			});
+	}
 });
 
 module.exports = newRouter;

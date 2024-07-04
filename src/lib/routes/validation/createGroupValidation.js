1fd9d6e840cca3df6e341a9a07db238e9c0b8248
Felix <fedecraft3312@gmail.com>
2024-07-03 22:08:41 -0300

    Publish edit

--- src/lib/routes/validation/createGroupValidation.js
+++ src/lib/routes/validation/createGroupValidation.js
@@ -4,10 +4,14 @@ const STRING_LENGTH = 256;
 const CREATE_GROUP_VALIDATION = [
 	body("name", "The name can't be empty").escape().notEmpty(),
 	body("name", "Name must be shorter than 256 characters").isLength({ max: STRING_LENGTH }),
-	body("description", "Description can't be empty").escape().notEmpty(),
+	// Cannot escape this one maybe with ejs '<%='
+	body("description", "Description can't be empty")
+		// .escape()
+		.notEmpty(),
 	body("description", "Description must be shorter than 8192 characters").isLength({ max: 8192 }),
 	body("category", "Category can't be empty").escape().notEmpty(),
-	body("url", "Bad url").escape(),
+	// Cannot escape this one maybe with ejs '<%='
+	// body("url", "Bad url").escape(),
 	body("url", "Url can't be longer than 1024 characters").isLength({ max: 1024 }),
 ];
 
--- src/routes/user/group/edit.js
+++ src/routes/user/group/edit.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { renderDataInternalErrorMessage } = require("../../../lib/status/messages");
 const expandData = require("../../../lib/misc/expandData");
+const CREATE_GROUP_VALIDATION = require("../../../lib/routes/validation/createGroupValidation");
+const { validationResult } = require("express-validator");
 
 const editRouter = express.Router();
 
@@ -25,11 +27,117 @@ editRouter.get("/:groupId", async (req, res) => {
 		});
 	} catch(err) {
 		console.error(err);
-		return res.status(500).render("status", {
-			...renderDataInternalErrorMessage,
-			...expandData(req),
-		});
+		return res
+			.status(500)
+			.render("status", {
+				...renderDataInternalErrorMessage,
+				...expandData(req),
+			});
 	}
 });
+
+editRouter.post(
+	"/:groupId",
+	CREATE_GROUP_VALIDATION,
+	async (req, res) => {
+		try {
+			const group = req.body;
+			
+			// Validate data
+			const result = validationResult(req);
+			if(!result.isEmpty()) {
+				const resultMessages = result.array();
+				
+				const messages = resultMessages.map((data) => {
+					return {
+						message: data.msg,
+						error: true,
+						type: "error"
+					}
+				});
+				req.flash("messages", messages);
+				
+				const SocialCategory = req.models.SocialCategory;
+				const categories = await SocialCategory.findAll();
+				return res.render("user/group/edit", {
+					title: group.name,
+					group,
+					categories,
+					...expandData(req),
+				});
+			}
+			
+			// Get group
+			const groupId = req.params.groupId;
+			const Groups = req.models.Groups;
+			const groupModel = await Groups.findOne({
+				where: {
+					id: groupId,
+				}
+			});
+			if(!groupModel) {
+				req.flash('messages', [{
+					message: "The group doesn't exists",
+					type: "error",
+				}]);
+				
+				return res
+					.redirect("/user/admin");
+			}
+			
+			
+			// Verify that this person is the author
+			const userId = req.user.id;
+			const isOwner = groupModel.userId === userId;
+			if(!isOwner) {
+				req.flash('messages', [{
+					// Hmm, this gives me the idea of a reputation app 😈😈
+					message: "You're not the owner of this property, reputation -1",
+					type: "error",
+					baddie: true,
+				}]);
+			
+				return res
+					.redirect("/user/admin");
+			}
+			
+			// Validate keys
+			// Fields were already validated we have to make sure there are no more than 4 only
+			if(Object.keys(group).length > 4) {
+				req.flash("messages", [{
+					message: "Incorrect object given",
+					type: "error",
+					// This won't happen without intervention
+					baddie: true,
+				}]);
+				
+				return res
+					.redirect("/user/admin");
+			}
+			
+			// Update data
+			for(const [key, value] of Object.entries(group)) {
+				groupModel[key] = value;
+			}
+			await groupModel.save();
+			
+			req.flash("messages", [{
+				message: "Property updated",
+				type: "success"
+			}]);
+			
+			return res
+				.redirect("/user/admin");
+		} catch(err) {
+			console.error(err);
+			return res
+				.status(500)
+				.render("status", {
+					...renderDataInternalErrorMessage,
+					...expandData(req),
+				});
+		}
+	}
+);
 
 module.exports = editRouter;

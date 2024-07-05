4519a0ec8cd8e6e53274f0d91637a69f5f43d289
Felix <fedecraft3312@gmail.com>
2024-07-05 10:37:07 -0300

    Geo search search bar and move marker to location

--- src/public/js/routes/user/meeti/create.js
+++ src/public/js/routes/user/meeti/create.js
@@ -1,68 +1,47 @@
 import { OpenStreetMapProvider, GeoSearchControl } from "leaflet-geosearch";
 
+// Map
 const londonCoordinates = [51.505, -0.09];
 const map = L.map('map').setView(londonCoordinates, 13);
 
-// let markers = new L.FeatureGroup().addTo(map);
-let marker = L.marker(londonCoordinates, {
+// Search provider
+const provider = new OpenStreetMapProvider();
+
+// Marker
+let markerLocation = L.marker(londonCoordinates, {
 	draggable: true,
 	autoPan: true,
 }).addTo(map)
 	.bindPopup('Meeti location')
 	.openPopup();
 
-/**
- * Locate
- */
-function locate(e) {
-	if(e.target.value.length > 8) {
-		const provider = new OpenStreetMapProvider();
-		
-		if(marker) {
-			map.remove(marker);
-		}
-		
-		// markers.clearLayers();
-		
-		// provider.search({
-		// 	query: e.target.value
-		// }).then((res) => {
-		// 	const first = res[0];
-		// 	const location = first.bounds[0];
-		// 	map.setView(location);
-			
-		// 	marker = new L.marker(location, {
-		// 		draggable: true,
-		// 		autoPan: true,
-		// 	}).addTo(map)
-		// 		.bindPopup(first.label)
-		// 		.openPopup();
-			
-		// 	markers.addLayer(marker);
-			
-		// 	marker.on("moveend", function(e) {
-		// 		const marker = e.target;
-		// 		const position = marker.getLatLng();
-		// 		map.panTo(new L.LatLng(
-		// 			position.lat,
-		// 			position.lng
-		// 		));
-				
-		// 		// TODO: Reverse geocoding
-		// 	});
-		// });
-	}
-}
-
+// Wait until the website loads
 document.addEventListener("DOMContentLoaded", () => {
+	// Create tile layer
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 	
+	// Insert search bar
 	const search = new GeoSearchControl({
-		provider: new OpenStreetMapProvider(),
-		style: "bar"
+		notFound: "The address you were looking for couldn't be found",
+		provider,
+		style: "bar",
+		showMarker: false,
 	});
-	
 	map.addControl(search);
+	
+	// Go to location
+	map.on("geosearch/showlocation", (e) => {
+		const place = e.location;
+		console.log(`Place: `, place);
+		const location = [place.y, place.x];
+		console.log(`Location: `, location);
+		
+		// Update marker
+		const placeName = place.label;
+		markerLocation.setLatLng(location)
+			.bindPopup(placeName)
+			.openPopup();
+	});
 });

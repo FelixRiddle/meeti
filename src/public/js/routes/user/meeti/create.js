f3475a8cc320a92629ae46a611d9b8185018891d
Felix <fedecraft3312@gmail.com>
2024-07-04 21:26:44 -0300

    Draggable marker

--- src/public/js/routes/user/meeti/create.js
+++ src/public/js/routes/user/meeti/create.js
@@ -1,9 +1,44 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+import { OpenStreetMapProvider } from "leaflet-geosearch";
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+const londonCoordinates = [51.505, -0.09];
+const map = L.map('map').setView(londonCoordinates, 13);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+let marker = L.marker(londonCoordinates, {
+	draggable: true,
+	autoPan: true,
+}).addTo(map)
+	.bindPopup('Meeti location')
+	.openPopup();
+
+/**
+ * Locate
+ */
+function locate(e) {
+	if(e.target.value.length > 3) {
+		const provider = new OpenStreetMapProvider();
+		
+		provider.search({
+			query: e.target.value
+		}).then((res) => {
+			const first = res[0];
+			const location = first.bounds[0];
+			map.setView(location);
+			
+			marker = new L.marker(location, {
+				draggable: true,
+				autoPan: true,
+			}).addTo(map)
+				.bindPopup(first.label)
+				.openPopup();
+		});
+	}
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+	
+	const browser = documnent.querySelector("#browser");
+	browser.addEventListener("input", locate);
+});

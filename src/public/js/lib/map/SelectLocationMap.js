9eeb074a11e4a8344108cc87b8522106d1011a0e
Felix <fedecraft3312@gmail.com>
2024-07-05 12:16:07 -0300

    Abstracted map into a class

---
+++ src/public/js/lib/map/SelectLocationMap.js
@@ -0,0 +1,126 @@
+import { OpenStreetMapProvider, GeoSearchControl } from "leaflet-geosearch";
+
+
+/**
+ * Select location map
+ */
+export default class SelectLocationMap {
+	updateCallback = undefined;
+	markerLocation = undefined;
+	
+	// Current information
+	address = undefined;
+	// London coordinates
+	lat = 51.505;
+	lng = -0.09;
+	
+	/**
+	 * 
+	 */
+	constructor() {
+		// Map
+		const londonCoordinates = [this.lat, this.lng];
+		const map = L.map('map').setView(londonCoordinates, 13);
+		
+		// Search provider
+		const provider = new OpenStreetMapProvider();
+		
+		// Marker
+		this.markerLocation = L.marker(londonCoordinates, {
+			draggable: true,
+			autoPan: true,
+		}).addTo(map)
+			.bindPopup('Meeti location')
+			.openPopup();
+		
+		const self = this;
+		
+		// Wait until the website loads
+		document.addEventListener("DOMContentLoaded", () => {
+			// Create tile layer
+			L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+			}).addTo(map);
+			
+			// Insert search bar
+			const search = new GeoSearchControl({
+				notFound: "The address you were looking for couldn't be found",
+				provider,
+				style: "bar",
+				showMarker: false,
+			});
+			map.addControl(search);
+			
+			// Go to location through the search
+			map.on("geosearch/showlocation", (e) => {
+				const place = e.location;
+				const lat = place.y;
+				const lng = place.x;
+				
+				self.updateMarker(lat, lng);
+			});
+			
+			// Update marker info
+			self.markerLocation.on("dragend", async (e) => {
+				const place = e.target;
+				const latLng = place._latlng;
+				
+				const lat = latLng.lat;
+				const lng = latLng.lng;
+				
+				await self.updateMarker(self, lat, lng);
+			});
+		});
+	}
+	
+	/**
+	 * Update marker
+	 */
+	async updateMarker(selectLocation, lat, lng) {
+		// Down to building information
+		const zoom = 18;
+		let extraParams = "";
+		
+		// Webpack environment plugin is not working
+		// // Append email if given
+		// const email = process.env.SERVICE_EMAIL;
+		// if(email) {
+		// 	extraParams += `&email=${email}`;
+		// }
+		
+		// We've got to reverse geocode the location
+		// Nominatim is free and does exactly that
+		const response = await fetch(
+			`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json&zoom=${zoom}${extraParams}`
+		);
+		
+		if(response.ok) {
+			const place = await response.json();
+			
+			// Update marker
+			const location = [lat, lng];
+			const placeName = place.display_name;
+			selectLocation.markerLocation.setLatLng(location)
+				.bindPopup(placeName)
+				.openPopup();
+			
+			// Place address
+			const address = place.address;
+			selectLocation.address = address;
+			
+			console.log(`Select location: `, selectLocation);
+			if(selectLocation.updateCallback) {
+				selectLocation.updateCallback();
+			}
+		}
+	}
+	
+	/**
+	 * Callback called on marker update
+	 */
+	setUpdateCallback(cb) {
+		this.updateCallback = () => cb({
+			address: this.address,
+		});
+	}
+}
--- src/public/js/routes/user/meeti/create.js
+++ src/public/js/routes/user/meeti/create.js
@@ -1,90 +1,10 @@
-import { OpenStreetMapProvider, GeoSearchControl } from "leaflet-geosearch";
+import SelectLocationMap from "../../../lib/map/SelectLocationMap";
 
-// Map
-const londonCoordinates = [51.505, -0.09];
-const map = L.map('map').setView(londonCoordinates, 13);
+const selectLocation = new SelectLocationMap();
 
-// Search provider
-const provider = new OpenStreetMapProvider();
-
-// Marker
-let markerLocation = L.marker(londonCoordinates, {
-	draggable: true,
-	autoPan: true,
-}).addTo(map)
-	.bindPopup('Meeti location')
-	.openPopup();
-
-/**
- * Update marker
- */
-async function updateMarker(lat, lng) {
-	// Down to building information
-	const zoom = 18;
-	let extraParams = "";
-	
-	// Webpack environment plugin is not working
-	// // Append email if given
-	// const email = process.env.SERVICE_EMAIL;
-	// if(email) {
-	// 	extraParams += `&email=${email}`;
-	// }
-	
-	// We've got to reverse geocode the location
-	// Nominatim is free and does exactly that
-	const response = await fetch(
-		`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json&zoom=${zoom}${extraParams}`
-	);
-	
-	if(response.ok) {
-		const place = await response.json();
-		
-		// Update marker
-		const location = [lat, lng];
-		const placeName = place.display_name;
-		markerLocation.setLatLng(location)
-			.bindPopup(placeName)
-			.openPopup();
-		
-		// Place address
-		const address = place.address;
-		console.log(`Place address: `, address);
-	}
-}
-
-// Wait until the website loads
-document.addEventListener("DOMContentLoaded", () => {
-	// Create tile layer
-	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
-	
-	// Insert search bar
-	const search = new GeoSearchControl({
-		notFound: "The address you were looking for couldn't be found",
-		provider,
-		style: "bar",
-		showMarker: false,
-	});
-	map.addControl(search);
-	
-	// Go to location through the search
-	map.on("geosearch/showlocation", (e) => {
-		const place = e.location;
-		const lat = place.y;
-		const lng = place.x;
-		
-		updateMarker(lat, lng);
-	});
-	
-	// Update marker info
-	markerLocation.on("dragend", async (e) => {
-		const place = e.target;
-		const latLng = place._latlng;
-		
-		const lat = latLng.lat;
-		const lng = latLng.lng;
-		
-		await updateMarker(lat, lng);
-	});
+selectLocation.setUpdateCallback(({
+	address,
+}) => {
+	// Fill inputs
+	console.log(`Address: `, address);
 });

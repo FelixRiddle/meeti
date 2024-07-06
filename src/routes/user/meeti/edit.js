74c2bcf9b46e31328982019384dcc933d2b87c11
Felix <fedecraft3312@gmail.com>
2024-07-06 19:56:29 -0300

    Edit page

---
+++ src/routes/user/meeti/edit.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const expandData = require("../../../lib/misc/expandData");
+
+const editRouter = express.Router();
+
+editRouter.get("/:meetiId", async (req, res) => {
+	try {
+		const meetiId = req.params.meetiId;
+		const {
+			Groups,
+			Meeti,
+		} = req.models;
+		
+		const groups = await Groups.findAll({
+			where: {
+				userId: req.user.id
+			}
+		});
+		const meetiModel = await Meeti.findByPk(meetiId);
+		const meeti = meetiModel.get({ raw: true });
+		
+		return res.render("user/meeti/edit", {
+			title: `Edit Meeti ${meeti.title}`,
+			...expandData(req),
+			groups,
+			meeti,
+		});
+	} catch(err) {
+		console.error(err);
+		return res.redirect("500");
+	}
+});
+
+module.exports = editRouter;
--- src/routes/user/meeti/index.js
+++ src/routes/user/meeti/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const createMeetiRouter = require("./create");
+const editRouter = require("./edit");
 
 const meetiRouter = express.Router();
 
+meetiRouter.use("/edit", editRouter);
 meetiRouter.use("/create", createMeetiRouter);
 
 module.exports = meetiRouter;

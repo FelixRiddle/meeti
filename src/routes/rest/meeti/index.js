f8aebd2f616bb9894c2ac8c1e11d24a10f9f5b79
Felix <fedecraft3312@gmail.com>
2024-07-14 09:05:37 -0300

    Added stylesheet

--- src/routes/rest/meeti/index.js
+++ src/routes/rest/meeti/index.js
@@ -13,14 +13,37 @@ meetiRouter.get("/:slug", async (req, res, next) => {
 	try {
 		const slug = req.params.slug;
 		
+		// Get complete meeti model
 		const meetiUtils = new MeetiUtils(req.models, req.user);
 		const completeMeetiModel = await meetiUtils.completeMeetiInformation(slug);
-		
 		if(!completeMeetiModel) {
 			return next();
 		}
 		
+		// Check if user participates in the meeti
 		const userParticipates = meetiUtils.userParticipates(completeMeetiModel);
+		
+		const {
+			Comment,
+			User
+		} = req.models;
+		
+		// Get meeti comments
+		const commentsModel = await Comment.findAll({
+			where: {
+				meetiId: completeMeetiModel.id,
+			},
+			order: [
+				["createdAt", "ASC"]
+			],
+			include: [{
+				model: User,
+				attributes: ["id", "name", "pfp"],
+			}],
+		});
+		
+		// Remove ORM properties
+		const comments = JSON.parse(JSON.stringify(commentsModel));
 		
 		const extra = await expandData(req);
 		return res.send({
@@ -28,6 +51,7 @@ meetiRouter.get("/:slug", async (req, res, next) => {
 			meeti: completeMeetiModel,
 			title: completeMeetiModel.title,
 			moment,
+			comments,
 			userParticipates,
 		});
 	} catch(err) {
